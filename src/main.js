e24df509c8d0c504d45c2445f280dc423b5910e3
Sergey Kravchenko <kravchenko.me@gmail.com>
2019-02-23 21:27:50 +0400

    Исправил внедрение в html

--- src/main.js
+++ src/main.js
@@ -2,29 +2,33 @@ import createFilter from './make-filter';
 import createCard from './make-task';
 
 const FILTERS_CONTAINER = document.querySelector(`.main__filter`);
-FILTERS_CONTAINER.innerHTML = ``;
-
 const CARDS_CONTAINER = document.querySelector(`.board__tasks`);
-CARDS_CONTAINER.innerHTML = ``;
 const CARDS_COUNT = 7;
 const TASK_FILTERS = [`all`, `overdue`, `today`, `favorites`, `repeating`, `tags`, `archive`];
 
 const getRandomInteger = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 
-TASK_FILTERS.forEach(function (item, i) {
-  FILTERS_CONTAINER.innerHTML += createFilter(
-      item,
-      getRandomInteger(0, 30),
-      i === 0 ? true : ``,
-      i === 1 || i === 2 ? true : ``
-  );
-}
-);
+createFilters();
+createCards(CARDS_COUNT);
 
 FILTERS_CONTAINER.addEventListener(`click`, (evt) => {
   evt.preventDefault();
   onFilterClick(evt);
 });
+
+function createFilters() {
+  FILTERS_CONTAINER.innerHTML = ``;
+  let filterTemplate = ``;
+  TASK_FILTERS.forEach(function (item, i) {
+    filterTemplate += createFilter(
+        item,
+        getRandomInteger(0, 30),
+        i === 0 ? true : ``,
+        i === 1 || i === 2 ? true : ``
+    );
+  });
+  FILTERS_CONTAINER.innerHTML = filterTemplate;
+}
 
 function onFilterClick(evt) {
   const target = evt.target;
@@ -40,11 +44,11 @@ function onFilterClick(evt) {
   }
 }
 
-createCards(CARDS_COUNT);
-
 function createCards(count) {
   CARDS_CONTAINER.innerHTML = ``;
+  let cardTemplate = ``;
   for (let i = 1; i <= count; i++) {
-    CARDS_CONTAINER.innerHTML += createCard(i);
+    cardTemplate += createCard(i);
   }
+  CARDS_CONTAINER.innerHTML = cardTemplate;
 }
